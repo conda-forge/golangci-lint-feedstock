3191cb95479a984551f0bd51f02c62f1ca9ddeb3
Daniel Nachun <daniel.nachun@gmail.com>
2025-09-29 00:42:14 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,13 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w -X main.version=${PKG_VERSION}" ./cmd/${PKG_NAME}
-go-licenses save ./cmd/${PKG_NAME} --save_path=license-files --ignore github.com/golangci/golangci-lint
+go-licenses save ./cmd/${PKG_NAME} --save_path=license-files \
+    --ignore github.com/golangci/golangci-lint \
+    --ignore github.com/ashanbrown/forbidigo \
+    --ignore github.com/ashanbrown/makezero
+
+# Manually copy licenses that go-licenses could not download
+cp -r ${RECIPE_DIR}/license-files/* ${SRC_DIR}/license-files
 
 if [[ ${build_platform} == ${target_platform} ]]; then
     mkdir -p ${PREFIX}/etc/bash_completion.d 
